0191f44456fadd3883161d445695956311756c18
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-01 11:31:55 -0600

    Created manufacturer relationships test

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -4,4 +4,8 @@
       it {should validate_presence_of :city}
       it {should validate_presence_of :year_founded}
     end
+
+    describe 'relationships' do
+      it {should have_many :cars}
+    end
   end
