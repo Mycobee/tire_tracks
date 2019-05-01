914a7f5f3b0e4d7b9a75b1957682823dcfd5e1ab
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-01 11:29:32 -0600

    Created passing validation tests

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,3 +1,5 @@
 class Manufacturer < ApplicationRecord
-   
+    validates :name, presence: true, uniqueness: true, length: { maximum: 75 }
+    validates :city, presence: true, uniqueness: true, length: { maximum: 75 }
+    validates :year_founded, presence: true, numericality: {greater_than: 1900, less_than: 2020, only_integer: true}
 end
--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -1,5 +1,7 @@
-describe 'validations', type: :model do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :city}
-    it {should validate_presence_of :year_founded}
+  RSpec.describe Manufacturer, type: :model do
+    describe 'validations' do
+      it {should validate_presence_of :name}
+      it {should validate_presence_of :city}
+      it {should validate_presence_of :year_founded}
+    end
   end
