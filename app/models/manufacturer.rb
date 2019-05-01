a1ddb8d0c93baf73ed61e794cddb0b1c205fa0ab
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-01 11:13:13 -0600

    Created manufacturer_spec and validation tests

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,3 +1,7 @@
 class Manufacturer < ApplicationRecord
-
+    describe 'validations', type: :model do
+      it {should validate_presence_of :name}
+      it {should validate_presence_of :city}
+      it {should validate_presence_of :year_founded}
+    end
 end
