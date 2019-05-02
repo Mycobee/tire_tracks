f0aee9e05a9b6ef6824dfafab5d8ab17627fffad
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-02 10:01:01 -0600

    Created #car_count instance method in manufacturer_spec

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -1,4 +1,6 @@
-  RSpec.describe Manufacturer, type: :model do
+require 'rails_helper'
+
+RSpec.describe Manufacturer, type: :model do
     describe 'validations' do
       it {should validate_presence_of :name}
       it {should validate_presence_of :city}
@@ -7,5 +9,16 @@
 
     describe 'relationships' do
       it {should have_many :cars}
+    end
+
+    describe 'instance methods' do
+      it '#car_count' do
+        manufacturer_1 = Manufacturer.create!(name: "Triumph", city: "London, UK", year_founded: 1913)
+
+        car_1a = manufacturer_1.cars.create!(name: "car1a", year: "1921")
+        car_1b = manufacturer_1.cars.create!(name: "car1b", year: "1922")
+
+        expect(manufacturer_1.car_count).to eq(2)
+      end
     end
   end
